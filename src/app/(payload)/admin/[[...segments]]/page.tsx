512191974afb195aa3f6bff0f732e7fb9fd479c3
Helmut Lindel <helmut@amua.app>
2026-02-04 09:19:35 +0100

    Fix config import: use sync config instead of configPromise, add missing HTTP methods

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
-import configPromise from '@payload-config'
+import config from '@payload-config'
 import { importMap } from '../importMap'
 
 type Args = {
@@ -13,9 +13,9 @@ type Args = {
 }
 
 export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
-  generatePageMetadata({ config: configPromise, params, searchParams })
+  generatePageMetadata({ config, params, searchParams })
 
 const Page = ({ params, searchParams }: Args) =>
-  RootPage({ config: configPromise, params, searchParams, importMap })
+  RootPage({ config, params, searchParams, importMap })
 
 export default Page
